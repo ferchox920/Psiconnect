a5a3e573fa943c8c29dc4ec4fd9a0dfbe113c774
ArceDaniel <jonathandanielarce9@gmail.com>
2023-01-30 17:10:13 -0300

    no se

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,18 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from "react";
 // import './App.css'
-import { io } from 'socket.io-client'
-import { Route, Routes } from 'react-router'
-import Home from './views/Home'
+import { io } from "socket.io-client";
+import { Route, Routes } from "react-router";
+import Home from "./views/Home";
 
 // const socket = io("http://localhost:3001");
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
-  
   return (
     <Routes>
-        <Route path='/' element={<Home />} />
-      </Routes>
-  
-  )
+      <Route path="/" element={<Home />} />
+    </Routes>
+  );
 }
-
 
 export default App;
