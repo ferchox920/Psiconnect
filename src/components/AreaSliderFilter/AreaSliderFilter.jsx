cf883095cd9daea572649e94356a494b0aef8cc6
jalejotorresm <alejandrotorres5286@gmail.com>
2023-01-31 16:20:14 -0500

    Componente Areas modificado

--- src/components/AreaSliderFilter/AreaSliderFilter.jsx
+++ src/components/AreaSliderFilter/AreaSliderFilter.jsx
@@ -1,8 +1,33 @@
-import style from './index.module.css'
+import { useEffect, useState } from 'react'
+import './AreaSliderFilter.css'
+import Areas from './Card/Areas.jsx'
+import { getAreas } from '../../features/apiPetitions'
 
 export default function AreaSliderFilter(){
+
+    const [areas, setAreas] = useState()
+
+    useEffect(
+        ()=>{
+            getAreas(setAreas)
+        }
+    , [])
+
     return(
-        <div className={style.container}>
+        <div className='areas'>
+            <div className='ondas'>
+                <img src="" alt="" />
+            </div>
+            <h1>Areas</h1>
+            <p>Trabajamos para ayudarte con retos personales como los siguientes:</p>
+            <div className='container'>
+                {areas.map(area => {
+                    return(
+                        <Areas img={area.image} area={area.area}/>
+                    )
+                })}
+            </div>
+            
         </div>
     )
 }
