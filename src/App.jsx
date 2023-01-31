85f10654aa510821ab6b26dcff7607b85a0b4ab0
rociodepoli <rociodepoli69@gmail.com>
2023-01-30 21:47:05 -0300

    login finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom';
+
 import RegisterPsico from './components/RegisterPsico/RegisterPsico';
 import { useEffect, useState } from 'react'
 import jwtDecode from 'jwt-decode'
@@ -8,25 +8,7 @@ import LoginUser from './components/LoginUser/LoginUser'
 
 // const socket = io("http://localhost:3001");
 function App() {
-  const [count, setCount] = useState(0)
-
-  function handleCredentialResponse(response){
-    console.log('response'+response);
-    const dataUser = jwtDecode(response.credential)
-    console.log(dataUser);
-  }
-
-  useEffect(()=>{
-    console.log(google);
-    google.accounts.id.initialize({
-      client_id: 'credenciales de google',
-      callback: handleCredentialResponse
-    });
-  },[])
-  google.accounts.id.renderButton(
-    document.getElementById('SignInDiv'),
-    {thema: 'inline', size:'large'}
-  )
+  
   return (
     <Routes>
         <Route path="/loginuser" element={<LoginUser/>} />
--- src/components/LoginUser/LoginUser.jsx
+++ src/components/LoginUser/LoginUser.jsx
@@ -1,11 +1,15 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { NavLink } from "react-router-dom";
+import jwtDecode from "jwt-decode";
+import { userLogin } from "../../features/apiPetitions";
+import style from './LoginUser.module.css'
 
 function validate(form) {
   let errors = {};
   if (!form.email) {
     errors.email = "Campo obligatorio";
-  }  if (!form.password) {
+  }
+  if (!form.password) {
     errors.password = "Campo obligatorio";
   }
   return errors;
@@ -20,6 +24,34 @@ export default function LoginUser() {
     email: "",
     password: "",
   });
+  const [showPassword , setShowPassword]= useState(false)
+
+  function handleCredentialResponse(response) {
+    console.log(response.credential);
+    const dataUser = jwtDecode(response.credential);
+    console.log(dataUser);
+    const body = {
+      email: dataUser.email,
+      password: `${dataUser.email}${dataUser.sub}`,
+    };
+    console.log(body);
+    userLogin(body);
+  }
+  useEffect(() => {
+    google.accounts.id.initialize({
+      client_id:
+        "299389682703-0bh5pf7g1r7c21169huc3s75rfdqr0cu.apps.googleusercontent.com",
+      callback: handleCredentialResponse,
+    });
+    google.accounts.id.renderButton(document.getElementById("SignInDiv"), {
+      thema: "inline",
+      size: "large",
+    });
+  }, []);
+
+  const viewPassword=()=>{
+
+  }
 
   const changeHandler = (e) => {
     e.preventDefault();
@@ -29,60 +61,68 @@ export default function LoginUser() {
 
   const submitHandler = (e) => {
     e.preventDefault();
-    setForm({email:'', password:''})
-window.alert("logged in")
-    }
-    ;
+    userLogin(form);
+    //setForm({email:'', password:''});
 
+    window.alert("logged in");
+  };
   return (
-    <div>
-      <form onSubmit={(e) => submitHandler(e)}>
-        <div>
-          <h1>Inicia sesión</h1>
-        </div>
-        <div>
-          <label>Email</label>
+    <div className={style.container}>
+      <div>
+        <form onSubmit={(e) => submitHandler(e)}>
           <div>
-            <input
-              type="text"
-              value={form.email}
-              name="email"
-              placeholder="Ingresa tu email"
-              onChange={(e) => changeHandler(e)}
-            />
-            {errors.email && <p>{errors.email}</p>}
+            <h1>Inicia sesión</h1>
           </div>
-          <label>Contraseña</label>
           <div>
-            <input
-              type="password"
-              value={form.password}
-              name="password"
-              placeholder="Ingresa tu contraseña"
-              onChange={(e) => changeHandler(e)}
-            />
-            {/* Acá iría el ojito para visualizar la contraseña */}
-            {errors.password && <p>{errors.password}</p>}
+            <label>Email</label>
+            <div>
+              <input
+                type="text"
+                value={form.email}
+                name="email"
+                placeholder="Ingresa tu email"
+                onChange={(e) => changeHandler(e)}
+              />
+              {errors.email && <p>{errors.email}</p>}
+            </div>
+            <label>Contraseña</label>
+            <div className={style.password}>
+              <input
+                type={ showPassword ? "text" : "password"}
+                value={form.password}
+                name="password"
+                placeholder="Ingresa tu contraseña"
+                onChange={(e) => changeHandler(e)}
+              />
+              <div className={style.pswicon} onClick={()=> setShowPassword(!showPassword)}>
+{showPassword ? (<img className={style.img} src="https://cdn-icons-png.flaticon.com/512/6866/6866733.png" alt="showPassword"/>) :
+<img className={style.img} src="https://cdn-icons-png.flaticon.com/512/6405/6405909.png" alt="nonShowPassword"/>}
+              </div>
+              {/* Acá iría el ojito para visualizar la contraseña */}
+              {errors.password && <p>{errors.password}</p>}
+            </div>
+            <NavLink to="/forgotpassword">
+              <button>Olvidé mi contraseña</button>
+            </NavLink>
           </div>
-          <NavLink to="/forgotpassword">
-            <button>Olvidé mi contraseña</button>
-          </NavLink>
-        </div>
-        <div>
-          {console.log(errors.email, errors.password)}
-          <input
-            type="submit"
-            disabled={
-              errors.email !== undefined || errors.password !== undefined
-            }
-            value={"Iniciar Sesión"}
-          ></input>
-        </div>
-      </form>
-      <p>
-        ¿Aún no tienes una cuenta?
-        <NavLink to="/registerUser">Regístrate aquí</NavLink>
-      </p>
+          <div>
+            {console.log(errors.email, errors.password)}
+            <input
+              type="submit"
+              disabled={
+                errors.email !== undefined || errors.password !== undefined
+              }
+              value={"Iniciar Sesión"}
+            ></input>
+          </div>
+          <p>
+            ¿Aún no tienes una cuenta?
+            <NavLink to="/registerUser">Regístrate aquí</NavLink>
+          </p>
+        </form>
+
+        <div id="SignInDiv" />
+      </div>
     </div>
   );
 }
