c75eacd6995f775df80937858ef75b0c15222a16
sonacho <nachokajita@gmail.com>
2023-02-01 11:01:22 -0300

    footer

--- src/components/AreaSliderFilter/AreaSliderFilter.jsx
+++ src/components/AreaSliderFilter/AreaSliderFilter.jsx
@@ -1,33 +1,36 @@
-import { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './AreaSliderFilter.css'
 import Areas from './Card/Areas.jsx'
 import { getAreas } from '../../features/apiPetitions'
 
 export default function AreaSliderFilter(){
 
-    const [areas, setAreas] = useState()
+    const [areas, setAreas] = useState(null)
 
-    useEffect(
-        ()=>{
-            getAreas(setAreas)
-        }
-    , [])
+    useEffect(()=>{
+        getAreas(setAreas)
+    }, [])
 
-    return(
-        <div className='areas'>
-            <div className='ondas'>
-                <img src="" alt="" />
+    if(areas) {
+        return(
+            <div className='areas'>
+                <div className='ondas'>
+                    <img src="" alt="" />
+                </div>
+                <h1>Areas</h1>
+                <p>Trabajamos para ayudarte con retos personales como los siguientes:</p>
+                <div className='container'>
+                    {areas.map((area) => {
+                        return(
+                            <Areas key={area.area} img={area.image} area={area.area}/>
+                        )
+                    })}
+                </div>
             </div>
-            <h1>Areas</h1>
-            <p>Trabajamos para ayudarte con retos personales como los siguientes:</p>
-            <div className='container'>
-                {areas.map(area => {
-                    return(
-                        <Areas img={area.image} area={area.area}/>
-                    )
-                })}
-            </div>
-            
-        </div>
-    )
+        )
+    }else{
+        return(
+            <div>loading</div>
+        )
+    }
 }
