9ecd6073ecb1bfc343cabac545fab0b6d2d69192
sonacho <nachokajita@gmail.com>
2023-02-01 18:13:04 -0300

    professionalCards

--- src/views/Professionals/Professionals.jsx
+++ src/views/Professionals/Professionals.jsx
@@ -2,6 +2,7 @@ import { useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import { getProfessionalByAreas } from "../../features/apiPetitions";
+import ProfessionalsCard from "./Card/ProfessionalsCard";
 
 export default function Professionals() {
   const { area } = useParams();
@@ -19,11 +20,12 @@ export default function Professionals() {
       type: "global",
     });
   }, [area]);
+  console.log(professionals)
 
   return (
-    <div>
-      {professionals?.map((e) => (
-        <h2>{e.name}</h2>
+    <div style={{ display: 'flex', flexWrap: 'wrap', height:'600px'}}>
+      {professionals && professionals.map((e,i) => (
+        <ProfessionalsCard  key={i} id={e.id} name={e.name} lastName={e.lastName} email={e.email} avatar={e.avatar}/>
       ))}
       {
         // barra de busqueda por nombre del profesional o especialdiad
