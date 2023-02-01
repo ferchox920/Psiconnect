baf153210c99397b47cf4c53302c2c7a242995e7
ferchox920 <fernandoramones92@gmail.com>
2023-02-01 14:42:32 -0300

    navBar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -2,22 +2,33 @@ import style from "./index.module.css";
 
 export default function NavBar() {
   return (
-      <>
-    <div className={style.container}>
+    <>
+      <div className={style.container}>
+        <img src="" alt="LOGO" />
         <Link to={""}>
           <h2>PSICONNECT</h2>
         </Link>
       </div>
       <div>
-        <Link to={""}>
-          <h3>Conocenos</h3>
+        <Link to={"/home"}>
+          <h3>¿Qué es psiconnect? </h3>
         </Link>
       </div>
-      <div >
-        <Link to={""}>
-          
-        </Link>
-    </div>
-      </>
+      <div>
+        <Link to={"/registerProfesional"}>¿Eres médico?</Link>
+      </div>
+      <div>
+        <Link to={"/AreasProfesional"}>Especialidades y servicios</Link>
+      </div>
+      <div>
+        <Link to={"/Asistencia"}>¿Cómo te ayudamos?</Link>
+      </div>
+      <div>
+        <Link to={""}>Inicio</Link>
+      </div>
+      <div>
+        <Link to={""}>Registrate</Link>
+      </div>
+    </>
   );
 }
