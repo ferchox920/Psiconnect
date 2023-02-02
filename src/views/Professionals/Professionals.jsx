bde490721acde90569750390d189654888cd799f
sonacho <nachokajita@gmail.com>
2023-02-02 15:02:08 -0300

    card

--- src/views/Professionals/Professionals.jsx
+++ src/views/Professionals/Professionals.jsx
@@ -19,23 +19,20 @@ export default function Professionals() {
       state: dispatch,
       area,
       type: "global",
-    })
+    });
   }, [area]);
   console.log(professionals)
 
   return (
-    <div style={{"margin-top":"14rem","margin-bottom":"14rem", "border":"2px solid red","width":"100%", "heigth":"auto", "display":"flex","flex-direction":"column","align-items":"center"}}>
- {/*    // barra de busqueda por nombre del profesional o especialdiad
-        // filtros por areas (reciclar componente) 
-*/}
-    <div style={{ display: 'flex', "flex-direction": 'column', "align-items":"center", height:'auto', width:'90%'}}>
+    <div style={{ display: 'flex', flexWrap: 'wrap', height:'600px'}}>
       {professionals && professionals.map((e,i) => (
         <ProfessionalsCard  key={i} id={e.id} name={e.name} lastName={e.lastName} email={e.email} avatar={e.avatar} skills={e.skills}/>
       ))}
       {
+        // barra de busqueda por nombre del profesional o especialdiad
+        // filtros por areas (reciclar componente)
         //todos los profesionales
       }
-    </div>
     </div>
   );
 }
