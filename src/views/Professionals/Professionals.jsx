912cdbb9cc38f8107c4423e3fc6061ae5f462446
braianpx <braivaldez16@gmail.com>
2023-02-02 20:26:44 -0300

    add new main

--- src/views/Professionals/Professionals.jsx
+++ src/views/Professionals/Professionals.jsx
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import { getProfessionalByAreas } from "../../features/apiPetitions";
 import { getAreas } from "../../features/apiPetitions"
+import ProfessionalsCard from './Card/ProfessionalsCard.jsx';
 
 export default function Professionals() {
   const { area } = useParams();
