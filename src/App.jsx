c6a1bcd277b4ebace90b5183fa9bdc0f2f6841fc
ferchox920 <fernandoramones92@gmail.com>
2023-02-03 16:50:17 -0300

    firtCommit

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,8 @@ import Footer from './components/Footer/Footer.jsx';
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { getUserByJWT } from './features/apiPetitions';
-import RegisterPsico from './components/RegisterPsico/RegisterPsico';
+import RegisterProfesional from './views/registerProfesional/RegisterProfesional';
+
 
 function App() {
   const dispacht = useDispatch();
@@ -23,7 +24,7 @@ function App() {
     <NavBar />
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path='/registerProfesional' element={<RegisterPsico />} />
+      <Route path='/registerProfesional' element={<RegisterProfesional />} />
       <Route path="/Details/:id" element={<Details />} />
       <Route path="/Professionals/:area" element={<Professionals />} />
     </Routes>
