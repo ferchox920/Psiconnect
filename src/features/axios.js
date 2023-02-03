72205d038927474025e31c0810a78c602e536c26
ArceDaniel <jonathandanielarce9@gmail.com>
2023-02-03 03:10:17 -0300

    deploy

--- src/features/axios.js
+++ src/features/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const instance = axios.create({
-    baseURL: "http://localhost:5000" //modifique la ruta que decia 5000 en vez de 3001
+    baseURL: "https://ph0-api-production.up.railway.app/" //modifique la ruta que decia 5000 en vez de 3001
 });
 
 export default instance;
