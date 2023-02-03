d9de9610ee98363753206f5e2b7b20c83d436a08
ArceDaniel <jonathandanielarce9@gmail.com>
2023-02-03 02:41:53 -0300

    asda

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -7,8 +7,8 @@ export default function Home(){
     return(
         <div>
             <Hero />
-            {/* <AreaSliderFilter />
-            <HomeInformations /> */}
+            <AreaSliderFilter />
+            <HomeInformations />
         </div>
     )
 }
