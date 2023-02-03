3caeb099dd12e074a3b295c5791157bb14ae5bc6
pablofarias1987 <pfgallardo1987@gmail.com>
2023-02-02 21:02:16 -0300

    3ra commit hero

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,25 +1,29 @@
 import style from './index.module.css'
-import React from "react";
-import { Link } from "react-router-dom";
+import React,{ useState }  from "react";
+import hero from "../../assets/hero.png"
+import FormModal from "../modals/Modals";
+
 
 export default function Hero(){
-
+    const [modal, setModal] = useState(null);
+    const openModal = () => {
+        setModal(true);
+      };
     return(
         //navbar
         <div>
         <div className={style.container}>
             <div>
             <h2>Registrate!</h2>
-            <img className={style.img} src="https://www.nicepng.com/png/full/125-1257197_mujer-feliz-personas-sonriendo-png.png"  alt="" />
+            <img className={style.img} src={hero}  alt="" />
             <h4>y Pide tu hora ya!! </h4><h4>Nuestros profesionales te atenderan 100% Online.</h4>
-            <Link to="/user/register">
-                <button>
+                <button onClick={openModal}>
                     Agendar
                 </button> 
-            </Link>
             </div>
         </div>
-        </div>
+        {modal && <FormModal name="User" set={setModal} />}
+        </div>      
         //footer
     )
 }
--- src/components/HomeIformations/HomeInformations.jsx
+++ src/components/HomeIformations/HomeInformations.jsx
@@ -1,5 +1,6 @@
 import style from "./index.module.css";
 
+
 export default function HomeInformations() {
   return (
     <div className={style.container}>
