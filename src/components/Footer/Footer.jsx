5d4ff19cab04818724d1d09f789d18be495d3282
Alejandro Torres <72758522+jalejotorresm@users.noreply.github.com>
2023-02-03 05:26:47 +0000

    Componentes revertidos a CSS Modules

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,15 +1,15 @@
-import './Footer.css'
+import style from './index.module.css'
 
 export default function Footer(){
     return(
-        <div className='container'>
-            <div className='left'>
+        <div className={style.container}>
+            <div className={style.left}>
                 <p>¿Necesitas ayuda?</p>
             </div>
-            <div className='center'>
+            <div className={style.center}>
                 <p>Copyright ©2023 Psiconnect</p>
             </div>
-            <div className='right'>
+            <div className={style.right}>
                 <p>Políticas de uso</p>
             </div>
         </div>
--- src/components/RegisterPsico/RegisterPsico.jsx
+++ src/components/RegisterPsico/RegisterPsico.jsx
@@ -2,7 +2,6 @@ import { useEffect, useState } from 'react';
 import { professionalRegister } from '../../features/apiPetitions';
 import { classPsicoRegister, spanError, inputError  } from './index.module.css';
 import  validationsForm  from './validator.js';
-import './RegisterPsico.css'
 
 export default function RegisterPsico() {
   
