4e424ce5f22774ce69ade2c5d6d5bbb3f5de409e
ArceDaniel <jonathandanielarce9@gmail.com>
2023-02-04 20:19:02 -0300

    agrege el compenente calendario

--- src/features/axios.js
+++ src/features/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const instance = axios.create({
-    baseURL: 'http://localhost:3001' //modifique la ruta que decia 5000 en vez de 3001
+    baseURL: 'http://localhost:5000' //modifique la ruta que decia 5000 en vez de 3001
 });
 
 export default instance;
--- src/views/Details/Details.jsx
+++ src/views/Details/Details.jsx
@@ -1,63 +1,67 @@
-import style from './Details.module.css'
+import style from "./Details.module.css";
 import { useEffect, useState } from "react";
-import LinesEllipsis from 'react-lines-ellipsis';
 import { useParams } from "react-router-dom";
 import { getProfessionalById } from "../../features/apiPetitions";
+import Calendary from "../../components/Calendary/Calendary";
 
 export default function Details() {
-
-
   const [profesional, setProfessional] = useState({});
-  console.log(profesional, 'aqui')
   const { id } = useParams();
 
-
   useEffect(() => {
-    getProfessionalById(id, setProfessional)
+    getProfessionalById(id, setProfessional);
   }, []);
 
-
-
   return (
-    <div className = {style.container}>
-
-     
-      <div className = {style.detailproffesional}>
-        <div className={style.imgageandtitle} >
-
+    <div className={style.container}>
+      <div className={style.detailproffesional}>
+        <div className={style.imgageandtitle}>
           <div>
-            <img className = {style.imageDetails} src= {profesional.avatar} alt="" />
+            <img
+              className={style.imageDetails}
+              src={profesional.avatar}
+              alt=""
+            />
           </div>
 
+          <div>
+            <h1>
+              {profesional.name}
+              {` ${profesional.lastName}`}
+            </h1>
+          </div>
+
+          <div className={style.subtitulo}>
+            <h3>Perfil profesional:</h3>
+          </div>
+
+          <div className={style.description}>
             <div>
-              <h1>{profesional.name}{` ${profesional.lastName}`}</h1>
+              <p className={style.paragraph}>
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi
+                in enim fringilla, auctor velit in, tempus massa. Aliquam id
+                arcu ut est ullamcorper dapibus quis et felis. Pellentesque
+                habitant morbi tristique senectus et netus et malesuada fames ac
+                turpis egestas. Proin dapibus erat malesuada tincidunt
+                ultricies. Integer id leo at odio egestas blandit. Integer
+                posuere consequat dapibus. Sed facilisis justo diam. Aliquam id
+                arcu ut est ullamcorper dapibus quis et felis. Pellentesque
+                habitant morbi tristique senectus et netus et malesuada fames ac
+                turpis egestas. Aliquam id arcu ut est ullamcorper dapibus quis
+                et felis. Pellentesque habitant morbi tristique senectus et
+                netus et malesuada fames ac turpis egestas.'
+              </p>
             </div>
 
-            <div className={style.subtitulo}>
-              <h3>Perfil profesional:</h3>
+            <div className={style.moreInfo}>
+              <button>Leer+</button>
             </div>
-
-            <div className={style.description}>
-              <div>
-             <p className={style.paragraph}>'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi in enim fringilla, auctor velit in, tempus massa. Aliquam id arcu ut est ullamcorper dapibus quis et felis. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Proin dapibus erat malesuada tincidunt ultricies. Integer id leo at odio egestas blandit. Integer posuere consequat dapibus. Sed facilisis justo diam. Aliquam id arcu ut est ullamcorper dapibus quis et felis. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aliquam id arcu ut est ullamcorper dapibus quis et felis. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.'</p>
-              </div>
-
-              <div className={style.moreInfo}>
-              <button >Leer+</button>
-             </div>
-            </div>
-
-            
-
+          </div>
+        </div>
       </div>
-
+      <div>
+        <Calendary />
       </div>
-
-
-
-
-
-
     </div>
   );
 }
