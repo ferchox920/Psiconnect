6dc8c7de466670cef3374160aedfded666516449
ArceDaniel <jonathandanielarce9@gmail.com>
2023-02-05 18:45:22 -0300

    arregle un import

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { getUserByJWT } from './features/apiPetitions';
 // import PostRegisterPsico from './components/postRegisterPsico/PostRegisterPsico';
-import RegisterProfesional from './views/RegisterProfesional/RegisterProfesional';
+// import RegisterProfesional from './views/RegisterProfesional/RegisterProfesional';
 import Asistencia from './views/Asistencia/Asistencia';
 
 function App() {
@@ -27,7 +27,7 @@ function App() {
     {/* <Route path='/profesional/postRegister' element={<PostRegisterPsico />} /> */}
       <Route path="/" element={<Home />} />
       <Route path='/Asistencia' element={<Asistencia />} />
-      <Route path='/registerProfesional' element={<RegisterProfesional />} />
+      {/* <Route path='/registerProfesional' element={<RegisterProfesional />} /> */}
       <Route path="/details/:id" element={<Details />} />
       <Route path="/Professionals" element={<Professionals />} />
       <Route path="/Professionals/:area" element={<Professionals />} />
