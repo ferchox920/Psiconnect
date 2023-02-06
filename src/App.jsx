a2e2176af5320e1fa83d5010367470505768956f
ArceDaniel <jonathandanielarce9@gmail.com>
2023-02-05 23:03:04 -0300

    mejore el manejo de errores del login/register de los usuarios

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,6 @@
 import "./App.css";
 import { Route, Routes } from "react-router";
 import Home from "./views/Home/Home";
-<<<<<<< HEAD
-import Details from './views/Details/Details';
-import Professionals from './views/Professionals/Professionals';
-import NavBar from './components/NavBar/NavBar';
-import Footer from './components/Footer/Footer.jsx';
-import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import { getUserByJWT } from './features/apiPetitions';
-// import PostRegisterPsico from './components/postRegisterPsico/PostRegisterPsico';
-// import RegisterProfesional from './views/RegisterProfesional/RegisterProfesional';
-import Asistencia from './views/Asistencia/Asistencia';
-=======
 import Details from "./views/Details/Details";
 import Professionals from "./views/Professionals/Professionals";
 import NavBar from "./components/NavBar/NavBar";
@@ -24,7 +12,6 @@ import { getUserByJWT } from "./features/apiPetitions";
 // import PostRegisterPsico from './components/postRegisterPsico/PostRegisterPsico';
 import RegisterProfesional from "./views/RegisterProfesional/RegisterProfesional";
 import Asistencia from "./views/Asistencia/Asistencia";
->>>>>>> devTesting
 
 function App() {
   const dispacht = useDispatch();
@@ -38,20 +25,6 @@ function App() {
   }, []);
   return (
     <>
-<<<<<<< HEAD
-    <NavBar />
-    <Routes>
-    {/* <Route path='/profesional/postRegister' element={<PostRegisterPsico />} /> */}
-      <Route path="/" element={<Home />} />
-      <Route path='/Asistencia' element={<Asistencia />} />
-      {/* <Route path='/registerProfesional' element={<RegisterProfesional />} /> */}
-      <Route path="/details/:id" element={<Details />} />
-      <Route path="/Professionals" element={<Professionals />} />
-      <Route path="/Professionals/:area" element={<Professionals />} />
-      
-    </Routes>
-    <Footer />
-=======
       <NavBar />
       <Routes>
         {/* <Route path='/profesional/postRegister' element={<PostRegisterPsico />} /> */}
@@ -63,7 +36,6 @@ function App() {
         <Route path="/Professionals/:area" element={<Professionals />} />
       </Routes>
       <Footer />
->>>>>>> devTesting
     </>
   );
 }
