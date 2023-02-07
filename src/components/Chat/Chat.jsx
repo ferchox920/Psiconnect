1790fbfe0468f585a6ef70b0602e9352b8e7f084
pablofarias1987 <pfgallardo1987@gmail.com>
2023-02-07 20:08:47 -0300

    chat css

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -1,11 +1,32 @@
-
+import React, { useState } from "react";
 import style from './Chat.module.css'
 
+
 export default function Chat(){
+    const [message, setMessage] = useState("");
+    const [messages, setMessages] = useState([]);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    }
 
     return(
         <div className={style.container}>
+      <div className={style.msgContainer}>
+        <p className={style.msg}>
+        Welcome {/*username */}!
+        </p>
 
-        </div>
-    )
-}
+
+      </div>
+      <form className={style.formContainer} >
+        <input
+          placeholder="Write here..."
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          />
+        <button className={style.btn}>Send</button>
+      </form>
+    </div>
+  );
+};
