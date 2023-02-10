ed19e2f36cd87640d0b511098da6a0ab8a8941b4
ArceDaniel <jonathandanielarce9@gmail.com>
2023-02-10 19:23:40 -0300

    finish home

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,34 +1,41 @@
-import style from './Hero.module.css'
-import React,{ useState }  from "react";
-import hero from "../../assets/hero.png"
+import style from "./Hero.module.css";
+import React, { useState } from "react";
+import hero from "../../assets/hero.png";
 import FormModal from "../modals/Modals";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
-
-export default function Hero(){
-    const [modal, setModal] = useState(null);
-    const openModal = () => {
-        setModal(!modal);
-      };
-    return(
-        //navbar
-        <>
-        <div className={style.container}>
-            <div className={style.heroContainer}>
-                <div>
-                    <img className={style.img} src={hero}  alt="" />
-                </div>
-                <div className={style.left}>
-                    <h2>Registrate!</h2>
-                    <h4>y Pide tu hora ya!! <br /> Nuestros profesionales te atenderan 100% Online. </h4>
-                    <button className={style.button} onClick={openModal}>
-                        Agendar
-                    </button> 
-                </div>
-            </div>
+export default function Hero() {
+  const [modal, setModal] = useState(null);
+  const user = useSelector((state) => state.user.user);
+  const navigate = useNavigate();
+  const openModal = () => {
+    user ?
+    navigate("/Asistencia")
+    :
+    setModal(!modal);
+  };
+  return (
+    //navbar
+    <>
+      <div className={style.container}>
+        <div className={style.heroContainer}>
+          <div>
+            <img className={style.img} src={hero} alt="" />
+          </div>
+          <div className={style.left}>
+            <h2>{!user ? "Registrate!" : "Agenda!"}</h2>
+            <h4>
+              y Pide tu hora ya!! <br /> Nuestros profesionales te atenderan
+              100% Online.{" "}
+            </h4>
+            <button className={style.button} onClick={openModal}>
+              Agendar
+            </button>
+          </div>
         </div>
-        {
-          modal && <FormModal name={'User'} />
-        }
-      </>
+      </div>
+      {modal && <FormModal name={"User"} />}
+    </>
   );
 }
