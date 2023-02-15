164861e25029cabb29d34cbe6e26dd9d6ee82ead
pablofarias1987 <pfgallardo1987@gmail.com>
2023-02-15 20:51:47 -0300

    primer commit de como te ayudamos

--- src/views/Asistencia/Asistencia.jsx
+++ src/views/Asistencia/Asistencia.jsx
@@ -11,9 +11,8 @@ const Asistencia = () => {
     <div className={style.countainer}>
       <h3 className={style.titulo}>Como te ayudamos </h3>
       <p className={style.parrafo}>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Obcaecati,
-        rerum. Molestiae deleniti illo aut error ipsam ullam quis veritatis
-        incidunt!
+      Contamos con gran variedad de profesionales a tu disposición para cada una de enfermedades 
+      de salud mental que puedas padecer!
       </p>
 
       <AreaSliderFilter/>
@@ -21,9 +20,8 @@ const Asistencia = () => {
       <br />
       <h3 className={style.titulo}>Elige el profesional de tu preferencia</h3>
       <p className={style.parrafo}>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Obcaecati,
-        rerum. Molestiae deleniti illo aut error ipsam ullam quis veritatis
-        incidunt!
+      Elige el profesional que más te acomode y si no tienes uno como referencia podrá elegir uno 
+      por habilidades o puntuación!
       </p>
     
       <Users />
