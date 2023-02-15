e76f8dcd876405d63d5ccf21f99bffda580a005a
ArceDaniel <jonathandanielarce9@gmail.com>
2023-02-15 14:14:18 -0300

    agrege el usuario admin

--- src/App.jsx
+++ src/App.jsx
@@ -57,7 +57,7 @@ function App() {
         <Route
           path="/professionalProfile/:section*"
           element={
-            <ProtectedRoute type={"prof"}>
+            <ProtectedRoute type={"professional"}>
               <ProSidebarProvider>
                 <ProfileProfessional />
               </ProSidebarProvider>
