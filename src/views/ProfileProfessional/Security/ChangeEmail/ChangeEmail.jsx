87f13833a0887f96dcb60593bb5ce56c5c6b44ad
Andrea Ramones <fernandoramones92@gmail.com>
2023-02-17 15:45:13 -0300

    commit: avance en cambio de email

--- src/views/ProfileProfessional/Security/ChangeEmail/ChangeEmail.jsx
+++ src/views/ProfileProfessional/Security/ChangeEmail/ChangeEmail.jsx
@@ -1,15 +1,13 @@
 import React, { useState } from 'react';
 
 function ChangeEmail() {
-  const [email, setEmail] = useState('');
+  const [currentEmail, setCurrentEmail] = useState('');
   const [newEmail, setNewEmail] = useState('');
-  const [verificationCode, setVerificationCode] = useState('');
-  const [showVerification, setShowVerification] = useState(false);
-  const [buttonDisabled, setButtonDisabled] = useState(false);
+  const [verifyNewEmail, setVerifyNewEmail] = useState('');
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (!email) {
+    if (!currentEmail) {
       alert("Debe ingresar su correo electrónico actual.");
       return;
     }
@@ -17,41 +15,14 @@ function ChangeEmail() {
       alert("Debe ingresar su nuevo correo electrónico.");
       return;
     }
-    if (!showVerification) {
+    if (!verifyNewEmail || verifyNewEmail !== newEmail) {
       alert("Debe verificar su nueva dirección de correo electrónico.");
       return;
     }
-    alert("¡Se cambió su correo electrónico correctamente!");
-  }
-
-  const handleSendVerification = (e) => {
-    e.preventDefault();
-    if (!newEmail) {
-      alert("Debe ingresar su nuevo correo electrónico.");
-      return;
-    }
-    // Enviar un correo electrónico con el código de verificación al usuario
-    setShowVerification(true);
-  }
-
-  const handleVerify = (e) => {
-    e.preventDefault();
-    if (!verificationCode) {
-      alert("Debe ingresar el código de verificación.");
-      return;
-    }
-    // Verificar el código de verificación y establecer el correo electrónico
-    setEmail(newEmail);
+    setCurrentEmail(newEmail);
     setNewEmail('');
-    setVerificationCode('');
-    setShowVerification(false);
-    alert("¡Se verificó su nueva dirección de correo electrónico!");
-  }
-
-  const toggleShowVerification = () => {
-    setShowVerification(!showVerification);
-    setButtonDisabled(true);
-    setTimeout(() => setButtonDisabled(false), 500);
+    setVerifyNewEmail('');
+    alert("¡Se cambió su correo electrónico correctamente!");
   }
 
   return (
@@ -62,8 +33,8 @@ function ChangeEmail() {
         <input
           type="email"
           id="current-email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
+          value={currentEmail}
+          onChange={(e) => setCurrentEmail(e.target.value)}
           required
         />
         <label htmlFor="new-email">Nuevo correo electrónico:</label>
@@ -75,35 +46,18 @@ function ChangeEmail() {
             onChange={(e) => setNewEmail(e.target.value)}
             required
           />
-          <button
-            type="button"
-            className="email-verification-button"
-            onClick={handleSendVerification}
-            disabled={buttonDisabled}
-          >
-            Enviar código de verificación
-          </button>
         </div>
-        {showVerification && (
-          <div className="email-verification-container">
-            <input
-              type="text"
-              id="verification-code"
-              placeholder="Código de verificación"
-              value={verificationCode}
-              onChange={(e) => setVerificationCode(e.target.value)}
-              required
-            />
-            <button
-              type="button"
-              className="email-verification-button"
-              onClick={handleVerify}
-              disabled={buttonDisabled}
-            >
-              Verificar
-            </button>
-          </div>
-        )}
+        <label htmlFor="verify-new-email">Verifica tu correo:</label>
+        <div className="email-verification-container">
+          <input
+            type="email"
+            id="verify-new-email"
+            value={verifyNewEmail}
+            onChange={(e) => setVerifyNewEmail(e.target.value)}
+            placeholder="Verificar nuevo correo electrónico"
+            required
+          />
+        </div>
         <button type="submit">Guardar cambios</button>
       </form>
     </div>
--- src/views/ProfileProfessional/Security/Security.jsx
+++ src/views/ProfileProfessional/Security/Security.jsx
@@ -1,11 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+import ChangeEmail from "./ChangeEmail/ChangeEmail";
 import ChangePassword from "./ChangePassword/ChangePassword";
 
 export default function Security() {
+  const [showPassword, setShowPassword] = useState(false);
+  const [showEmail, setShowEmail] = useState(false);
+
+  const handleShowPassword = () => {
+    setShowPassword(true);
+    setShowEmail(false);
+  };
+
+  const handleShowEmail = () => {
+    setShowPassword(false);
+    setShowEmail(true);
+  };
+
   return (
     <>
-    <ChangePassword/>
-    <ChangeEmail/>
+      <div>
+        <br />
+        <button onClick={handleShowPassword}>Cambiar contraseña</button>
+        <button onClick={handleShowEmail}>Cambiar correo electrónico</button>
+      </div>
+
+      {showPassword && <ChangePassword />}
+      {showEmail && <ChangeEmail />}
     </>
   );
 }
