e9f5253caabbc3230664529070f09d22b0edd9ce
ArceDaniel <jonathandanielarce9@gmail.com>
2023-02-20 01:21:15 -0300

    das

--- src/features/apiPetitions.js
+++ src/features/apiPetitions.js
@@ -208,8 +208,8 @@ export async function getProfessionalsFilters({
       ? state(peticion?.data)
       : state(setFilterProfessional(peticion?.data));
   } catch (error) {
-    errorMenssage(`no se encontro a ${name} ${lastName? lastName:''}`)
-    return error.response;
+    errorMenssage(`no se encontro a ${name} ${lastName ? lastName : ""}`);
+    throw new Error("error");
   }
 }
 
@@ -257,8 +257,8 @@ export async function requestConsultation(body) {
     console.log(peticion.data.data.links[1].href);
     return peticion.data.data.links[1].href;
   } catch (error) {
-    errorMenssage('Upps algo salio mal en nuestros sistemas')
-    throw new Error('upps')
+    errorMenssage("Upps algo salio mal en nuestros sistemas");
+    throw new Error("upps");
   }
 }
 
@@ -338,21 +338,21 @@ export async function getResultProfessionalPayments(professionalId, state) {
   }
 }
 
-export async function getUserPayments(userId, state){
+export async function getUserPayments(userId, state) {
   try {
-    const response = await axios.get(`/payment/user/${userId}`)
-    return state(response?.data)
+    const response = await axios.get(`/payment/user/${userId}`);
+    return state(response?.data);
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
 }
 
-export async function getResultUserPayments(userId, state){
+export async function getResultUserPayments(userId, state) {
   try {
-    const response = await axios.get(`/payment/userPayment/${userId}`)
-    return state(response?.data)
+    const response = await axios.get(`/payment/userPayment/${userId}`);
+    return state(response?.data);
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
 }
 // export default async function postImageCloudinary(file, image) {
