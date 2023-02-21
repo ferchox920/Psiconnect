06e976278b61cc186380ef4f407d7bad8a90bafb
Andrea Ramones <fernandoramones92@gmail.com>
2023-02-21 17:29:07 -0300

    commit

--- src/views/ProfileUser/UsersForm/UsersForm.jsx
+++ src/views/ProfileUser/UsersForm/UsersForm.jsx
@@ -1,126 +1,129 @@
-import React from 'react'
-import {useState} from 'react'
-import { useSelector } from 'react-redux'
-import putUserData from '../../../features/apiPetitions.js'
-import swal from "sweetalert";
-import { useParams } from 'react-router-dom'
-import style from './UsersForm.module.css'
+import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
+import swal from 'sweetalert';
+import putUserData from '../../../features/apiPetitions.js';
+import style from './UsersForm.module.css';
 
-export default function UsersForm (){
+export default function UsersForm() {
+  const users = useSelector((state) => state.user.user);
+  const [file, setFile] = useState(null);
+  const [urlImage, setUrlImage] = useState(null);
+  const { id } = useParams();
 
-  const users = useSelector((state) => state.user.user)
-  const [ file, setFile ] = useState(null)
-  const [urlImage, setUrlImage] = useState(null)
-  const {id} = useParams()
-    
-  const [ error, setError ] = useState({
-      name : '',
-      lastName: '',
-      phone: '',
-      image: ''
-  })
+  const [error, setError] = useState({
+    phone: '',
+  });
 
   const [input, setInput] = useState({
-    name : '',
-    lastName: '',
+    name: users.name,
+    lastName: users.lastName,
     phone: '',
-    image: ''
-  })
+    image: '',
+  });
 
   const handleInputChanges = (e) => {
     setInput({
       ...input,
       [e.target.name]: e.target.value,
-      
     });
-    
+
     setError(validation({
-        ...input,
-        [e.target.name]: e.target.value,
-      }))
-    
+      ...input,
+      [e.target.name]: e.target.value,
+    }));
   };
 
-  const handleSubmitUpdate = (e) => {
-      e.preventDefault()
-      if(Object.entries(error).length == 0){
-        putUserData({...input , id: users.id})
-        swal({
-          title: "Cambios guardados!",
-          text: `Sus datos fueron actualizados correctamente`,
-          icon: "success",
-        })
-        
-      } else 
-      swal({
-        title: "Error!",
-        text: Object.values(error)[0],
-        icon: "error",
-      })
+  const handleSubmitUpdate = async (e) => {
+    e.preventDefault();
 
-      setInput({
-        name : '',
-        lastName: '',
-        phone: '',
-        image: ''
-      })
-      alert('Sus datos han sido actualizados')
-  }
+    if (Object.entries(error).length === 0) {
+      try {
+        const updatedUser = await putUserData({ ...input, id: users.id });
+        swal({
+          title: 'Cambios guardados!',
+          text: 'Sus datos fueron actualizados correctamente',
+          icon: 'success',
+        });
+      } catch (err) {
+        swal({
+          title: 'Error!',
+          text: 'No se pudo actualizar sus datos. Inténtelo de nuevo más tarde',
+          icon: 'error',
+        });
+      }
+    } else {
+      swal({
+        title: 'Error!',
+        text: Object.values(error)[0],
+        icon: 'error',
+      });
+    }
+
+    setInput({
+      name: '',
+      lastName: '',
+      phone: '',
+      image: '',
+    });
+  };
 
   const handledImageProfile = (e) => {
-    if(!e.target.files[0])return
+    if (!e.target.files[0]) return;
+
     setInput({
       ...input,
       [e.target.name]: e.target.files[0],
       image: URL.createObjectURL(e.target.files[0]),
-    })
-  }
+    });
+  };
 
   const handledChangeImage = (e) => {
-    setInput ({
+    setInput({
       ...input,
-      image : ''
-    })
-    let img = document.querySelector('#imageProfile')
-    img.value = ''
-  }
-    
+      image: '',
+    });
+
+    let img = document.querySelector('#imageProfile');
+    img.value = '';
+  };
+
   return (
-      <div  className={style.profileContainer}>
-        <section className={style.profileTitle}>
-          <p className={style.p}>
-            Estos son tus datos de usuario<br/>
-            (puedes modificarlos si lo necesitas)
-          </p>
-        </section>
-        
-        <form className={style.profileForm} onSubmit ={e => handleSubmitUpdate(e)}>
-          <div className={style.changesField}>
-            <section className={style.imageChange}>
-              <label className={style.avatarTitle}>Tu foto de perfil</label>
-              <div className={style.imageContainer}>
-                <img className={style.sourceImage} src={users.image || input.image}/>
-              </div>
+    <div className={style.profileContainer}>
+      <section className={style.profileTitle}>
+        <p className={style.p}>
+          Estos son tus datos de usuario
+          <br />
+          (puedes modificarlos si lo necesitas)
+        </p>
+      </section>
 
-              <input
-                  className={style.imageInput} 
-                  type= 'file' 
-                  name = 'image'
-                  id = '#imageProfile'
-                  onChange = {e => handledImageProfile(e)}
-              />
-            </section>          
+      <form className={style.profileForm} onSubmit={handleSubmitUpdate}>
+        <div className={style.changesField}>
+          <section className={style.imageChange}>
+            <label className={style.avatarTitle}>Tu foto de perfil</label>
+            <div className={style.imageContainer}>
+              <img className={style.sourceImage} src={users.image || input.image} />
+            </div>
 
-            <section className={style.dataChange}>
-              <input
-                className={style.dataInput} 
-                type="text" 
-                placeholder='Nombres' 
-                name = 'name'
-                value = {users?.name}
-                disabled
-                onChange= {e => handleInputChanges(e)}
-              />
+            <input
+              className={style.imageInput}
+              type='file'
+              name='image'
+              id='imageProfile'
+              onChange={handledImageProfile}
+            />
+          </section>
+
+          <section className={style.dataChange}>
+            <input
+              className={style.dataInput}
+              type='text'
+              placeholder='Nombres'
+              name='name'
+              value={input.name}
+              disabled
+            />
 
               <input
                 className={style.dataInput}  
