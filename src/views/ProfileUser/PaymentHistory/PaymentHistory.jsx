5880aa88c97606e182c1bb803a214707997c6a91
jalejotorresm <alejandrotorres5286@gmail.com>
2023-02-21 10:30:10 -0500

    Seccion Pagos Usuario Actualizada

--- src/views/ProfileUser/PaymentHistory/PaymentHistory.jsx
+++ src/views/ProfileUser/PaymentHistory/PaymentHistory.jsx
@@ -2,42 +2,43 @@ import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import {  getUserPayments, getResultUserPayments } from "../../../features/apiPetitions";
 import Card from "./Card/Card.jsx";
-import style from "./PaymentHistory.module.css"; // Importar el archivo de estilos
+import style from "./PaymentHistory.module.css";
 
 
 export default function PaymentHistory () {
-    const users = useSelector((state) => state.user.user)
+  const [consults, setConsults] = useState()
+  const userId = useSelector((state) => state.user.user.id)
 
   useEffect(() => {
     getUserPayments(userId, setConsults);
-    getResultUserPayments(userId, setPay)
+    // getResultUserPayments(userId, setPay)
   }, []);
 
 
   return (
-    <div className={style.paymentsContainer}> {/* Aplicar la clase CSS en el contenedor principal */}
-      <div className={style.paymentsTitle}>Historial de pagos</div>
-      <div className={style.paymentsListContainer}> {/* Agregar un contenedor para la lista de consultas */}
+    <div className={style.paymentsContainer}> 
+      <section className={style.paymentTitle}>
+        <p className={style.title}>
+          <b>Historial de pagos</b>
+        </p> 
+      </section>
+
+      <section className={style.paymentList}>
         {consults &&
           consults.map((c, i) => {
             return (
-              <div>
+              <div className={style.paymentInfo}>
                 <Card key={i} consult={c} />
               </div>
             );
-          })}
-        {!consults?.length && <div  className={style.noPymnt}>
-          <p> <b>No tienes pagos realizados</b> </p>
-        </div> }
-      </div>
-      {/* <div className={style.paymentsPayContainer}>  Agregar un contenedor para el monto total 
-        {pay ? (
-          <>
-            <p className={style.paymentsPayTitle}>En total tus pagos suman</p>
-            <p className={style.paymentsPayAmount}>{pay}</p>
-          </>
-        ) : null}
-      </div> */}
+        })}
+
+        {!consults?.length && <div className={style.noPymnt}>
+          <p>
+            <b>No tienes pagos realizados</b>
+          </p>
+        </div>}
+      </section>
     </div>
   );
 }
