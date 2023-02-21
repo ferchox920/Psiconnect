eb83e690fceca0a217951ad6f6dd424e8f663202
Andrea Ramones <fernandoramones92@gmail.com>
2023-02-21 03:16:52 -0300

    commit

--- src/views/ProfileUser/ApptHistory/Card/Card.jsx
+++ src/views/ProfileUser/ApptHistory/Card/Card.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { getProfessionalById } from "../../../../features/apiPetitions";
 import style from "./Card.module.css";
 
